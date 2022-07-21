4aa00d02f32d89a170d9c2ba92d295108005b0c5
jwcamelo <jwcmade@gmail.com>
2022-07-21 17:07:25 -0300

    add custom Email validator to Update Cliente

---
+++ src/main/java/com/jwcamelo/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,48 @@
+package com.jwcamelo.cursomc.services.validation;
+
+import com.jwcamelo.cursomc.domain.Cliente;
+import com.jwcamelo.cursomc.domain.enums.TipoCliente;
+import com.jwcamelo.cursomc.dto.ClienteDTO;
+import com.jwcamelo.cursomc.dto.ClienteNewDTO;
+import com.jwcamelo.cursomc.repositories.ClienteRepository;
+import com.jwcamelo.cursomc.resources.exception.FieldMessage;
+import com.jwcamelo.cursomc.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+    @Autowired
+    private HttpServletRequest request;
+    @Autowired
+    private ClienteRepository repo;
+    @Override
+    public void initialize(ClienteUpdate ann){}
+
+    public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context){
+
+        Map<String,String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        Integer uriId = Integer.parseInt(map.get("id"));
+
+        List<FieldMessage> list = new ArrayList<>();
+        //testes
+
+        Cliente aux = repo.findByEmail(objDto.getEmail());
+        if(aux != null && !aux.getId().equals(uriId)) list.add(new FieldMessage("email","Email já está em uso no sistema"));
+
+        for(FieldMessage e : list){
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage())
+                    .addPropertyNode(e.getFieldName()).addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+
+}
