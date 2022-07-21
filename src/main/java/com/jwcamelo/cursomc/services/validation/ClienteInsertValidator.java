93cd3fcd5764a7c95f99a191aa8a32e37adee0d9
jwcamelo <jwcmade@gmail.com>
2022-07-21 16:48:23 -0300

    add Email custom validation to insert Cliente

--- src/main/java/com/jwcamelo/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/jwcamelo/cursomc/services/validation/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package com.jwcamelo.cursomc.services.validation;
 
+import com.jwcamelo.cursomc.domain.Cliente;
 import com.jwcamelo.cursomc.domain.enums.TipoCliente;
 import com.jwcamelo.cursomc.dto.ClienteNewDTO;
+import com.jwcamelo.cursomc.repositories.ClienteRepository;
 import com.jwcamelo.cursomc.resources.exception.FieldMessage;
 import com.jwcamelo.cursomc.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -11,6 +14,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository repo;
     @Override
     public void initialize(ClienteInsert ann){}
     public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context){
@@ -21,6 +27,9 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
         if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj()))
             list.add(new FieldMessage("CpfOuCnpj","CNPJ inválido"));
+
+        Cliente aux = repo.findByEmail(objDto.getEmail());
+        if(aux != null) list.add(new FieldMessage("email","Email já está em uso no sistema"));
 
         for(FieldMessage e : list){
             context.disableDefaultConstraintViolation();
